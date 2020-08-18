3d56af327ed12ac8c24f858e4519463198dca5d8
André <andreboerger95@gmail.com>
2020-08-18 18:28:43 +0200

    splunk

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,9 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import com.example.demo.SareetaApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +23,9 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
-	
+
+	private static final Logger log = LoggerFactory.getLogger(SareetaApplication.class.getName());
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -33,10 +37,12 @@ public class OrderController {
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("User was not found by order!");
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		log.info("Order successfully submitted by user " + username);
 		return ResponseEntity.ok(order);
 	}
 	
